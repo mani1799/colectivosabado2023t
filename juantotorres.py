ee4041eb84722ca232add0a509ce346dadd00642
juan esteban torres ibarguen <jt158161@gmail.com>
2023-03-25 11:16:00 -0500

    nt7 se crea archivo con mi nombre

--- juantotorres.py
+++ juantotorres.py
@@ -2,4 +2,5 @@ from os import system
 
 print("Hola soy juan torres")
 
-system("C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe")
+while True:
+    system("C:/Program Files/Google/Chorme/Application/chorme.exe")
